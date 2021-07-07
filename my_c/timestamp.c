a7bbbc1e8e6215fe17e25b1d2fa00210fafea927
Vitah Lin <linw1225@gmail.com>
2021-07-07 16:11:18 +0800

    Feat：C获取时间戳

---
+++ my_c/timestamp.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+#include <sys/time.h>
+
+double getcurrentSecond() {}
+
+int main() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    printf("秒: %ld\n", tv.tv_sec);
+    printf("毫秒: %ld\n", tv.tv_sec * 1000 + tv.tv_usec / 1000);
+    printf("微秒: %ld\n", tv.tv_sec * 1000000 + tv.tv_usec);
+
+    return 0;
+}
