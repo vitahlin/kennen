b291ce50f13d13fab7f21bbab219e894cd2dcb2d
Vitah Lin <linw1225@gmail.com>
2021-07-29 20:56:42 +0800

    Feat: 封装close函数

--- my_c/unp/lib/unp.h
+++ my_c/unp/lib/unp.h
@@ -1,5 +1,8 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int wrapSocket(int domain, int type, int protocol);
+
+void wrapClose(int sock_fd);
--- my_c/unp/lib/wrap_socket.c
+++ my_c/unp/lib/wrap_socket.c
@@ -11,9 +11,22 @@
 int wrapSocket(int domain, int type, int protocol) {
     int n;
     if ((n = socket(domain, type, protocol)) < 0) {
-        perror("socket error");
+        perror("socket error ");
         exit(-1);
     }
 
     return n;
+}
+
+
+/**
+ * 封装的close函数
+ * @param sock_fd
+ */
+void wrapClose(int sock_fd) {
+    if (close(sock_fd) == 0) {
+        return;
+    }
+    perror("close error ");
+    exit(-1);
 }
